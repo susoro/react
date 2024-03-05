9536ab927189ade28cdeb2d1355ee887c22e4d7c
sugyeongsu <78544372+susoro@users.noreply.github.com>
2024-03-05 23:05:20 +0900

    Update App.js

--- react_end/src/App.js
+++ react_end/src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 function App() {
 
   let post = '종로 쌍화차 맛집';
-  let [a,b] = useState('정현시와 함께하는 삼각형 여행기');
+  let [a,b] = useState(['정현시와 함께하는 삼각형 여행기',20240305]);
   let [c,d] = useState('두바이 벼락부자 vs 사주로 움직이는 수경수');
   let [e,f] = useState('수경수의 미래를 예측해보자!');
 // useState 변동시 자동으로 html 반영되게 만들고 싶으면 쓰면된당
